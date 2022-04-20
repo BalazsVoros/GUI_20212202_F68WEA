bfdda969f66db452c0cda648a93b3379ec44b2e1
BalazsVoros <balazs.t.voros@gmail.com>
2022-04-20 19:35:38 +0200

    created the game xaml file

--- Views/GameWindowView.xaml.cs
+++ Views/GameWindowView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NIKTOPIA.Controllers;
+using NIKTOPIA.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,30 @@ namespace NIKTOPIA.Views
     /// </summary>
     public partial class GameWindowView : UserControl
     {
+        GameController gameController;
+
         public GameWindowView()
         {
             InitializeComponent();
+            GameLogic gameLogic = new GameLogic();
+            display.SetupModel(gameLogic);
+            gameController = new GameController(gameLogic);
+            display.Size = new NIKTOPIA.Misc.Size(Application.Current.MainWindow.ActualWidth, Application.Current.MainWindow.ActualHeight);
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // display. .Resize(new NIKTOPIA.Misc.Size(grid.ActualWidth, grid.ActualHeight));
+            var window = Window.GetWindow(this);
+            window.KeyDown += Window_KeyDown;
+            display.InvalidateVisual();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            gameController.KeyPressed(e.Key);
+            display.InvalidateVisual();
+        }
+
     }
 }
