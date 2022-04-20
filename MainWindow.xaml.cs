d2f3c5d7cffee54aa4d02c8938cc1742c371d1a3
BalazsVoros <balazs.t.voros@gmail.com>
2022-04-20 16:57:21 +0200

    implemented datatemplates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NIKTOPIA.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,7 @@ namespace NIKTOPIA
         public MainWindow()
         {
             InitializeComponent();
-
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
