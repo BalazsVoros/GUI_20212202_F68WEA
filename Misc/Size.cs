8a95faba1fc0e53638e0e9a02a75c56c8e73efca
BalazsVoros <balazs.t.voros@gmail.com>
2022-04-20 19:34:31 +0200

    created misc folder with size class

---
+++ Misc/Size.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIKTOPIA.Misc
+{
+    public class Size
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Size(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
